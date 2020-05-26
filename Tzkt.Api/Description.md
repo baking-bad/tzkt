fa0c168aaa91d90032e76f981b34f190a84dfba5
257Byte <257byte@gmail.com>
2020-05-26 14:29:01 +0300

    Update documentation description

---
+++ Tzkt.Api/Description.md
@@ -0,0 +1,44 @@
+TzKT Explorer provides a free API for accessing detailed Tezos blockchain data and to help developers build more services and applications on top of Tezos.
+TzKT API is an open-source project, so you can clone and build it and use as a self-hosted service to avoid any risks of using third-party services.
+
+Feel free to contact us if you have any questions or if need any specific features or endpoints or query parameters, etc.
+Any feedback really helps us make TzKT better.
+And don't forget to star TzKT [on GitHub](https://github.com/baking-bad/tzkt) :)
+
+### Terms of Use
+
+TzKT API is free for everyone and for both commercial and non-commercial usage.
+
+If your application or service uses the TzKT API in any forms: directly on frontend or indirectly on backend, you should mention that fact on your website or application
+by placing the label **"Powered by TzKT API"** with a direct link to [tzkt.io](https://tzkt.io).
+
+### Supported Tezos Networks
+
+TzKT API is available for the following Tezos networks and on the following base URLs:
+
+- Mainnet: `https://api.tzkt.io/` ([check](https://api.tzkt.io/v1/head))
+- Babylonnet: `https://api.babylon.tzkt.io/` ([check](https://api.babylon.tzkt.io/v1/head))
+- Carthagenet: `https://api.carthage.tzkt.io/` ([check](https://api.carthage.tzkt.io/v1/head))
+- Zeronet: `https://api.zeronet.tzkt.io/` ([check](https://api.zeronet.tzkt.io/v1/head))
+
+### Staging environment
+
+We also provide a staging environment with the newest features and updates before deploying to production.
+This allows to test latest features and pre-update client applications in case of breaking changes
+so API may be used without the adverse consequences of operating in a production environment.
+
+Staging API: `https://staging.api.tzkt.io/` ([check](https://staging.api.tzkt.io/v1/head))
+
+### Rate Limits
+
+There will be no strict rate limits as long as our servers can handle the load without additional infrastructure costs.
+However, any apparent abuse will be prevented by setting targeted rate limits.
+
+### Contacts Us
+
+Feel free to ask any questions about TzKT Explorer and Tezos in general. Our team is always happy to help!
+
+- Email: hello@baking-bad.org
+- Twitter: https://twitter.com/TezosBakingBad
+- Telegram: [tg://resolve?domain=baking_bad_chat](tg://resolve?domain=baking_bad_chat)
+- Slack: https://tezos-dev.slack.com/archives/CV5NX7F2L
--- Tzkt.Api/Startup.cs
+++ Tzkt.Api/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -8,7 +9,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 using Tzkt.Api.Repositories;
 using Tzkt.Api.Services.Cache;
@@ -64,24 +64,20 @@ namespace Tzkt.Api
 
             services.AddOpenApiDocument(options =>
             {
-                options.DocumentName = "v1-preview";
+                options.DocumentName = "v1.0";
                 options.PostProcess = document =>
                 {
                     document.Info.Title = "TzKT API";
-                    document.Info.Description = "TzKT Explorer provides an API for accessing the extended Tezos blockchain data. This API is free for both commercial and non-commercial usage. " +
-                        "Also, TzKT API is an open-source project, so you can clone and build it and use as a self-hosted service to avoid risks of using third-party services. " +
-                        "Feel free to contact us if you need specific features or endpoints or query parameters, etc.\n\n" +
-                        "TzKT API is available for the following Tezos networks:\n\n" +
-                        "Mainnet: `https://api.tzkt.io/`\n\n" + 
-                        "Babylonnet: `https://api.babylon.tzkt.io/`\n\n" +
-                        "Carthagenet: `https://api.carthage.tzkt.io/`\n\n" +
-                        "Zeronet: `https://api.zeronet.tzkt.io/`";
-                    document.Info.Version = "v1-preview";
+
+                    if (File.Exists("Description.md"))
+                        document.Info.Description = File.ReadAllText("Description.md");
+
+                    document.Info.Version = "v1.0";
                     document.Info.Contact = new NSwag.OpenApiContact
                     {
                         Name = "Baking Bad Team",
                         Email = "hello@baking-bad.org",
-                        Url = "https://tzkt.io"
+                        Url = "https://baking-bad.org/docs"
                     };
                     document.Info.ExtensionData = new Dictionary<string, object>
                     {
@@ -110,7 +106,7 @@ namespace Tzkt.Api
             app.UseOpenApi(options => 
             {
                 options.Path = "/v1/swagger.json";
-                options.DocumentName = "v1-preview";
+                options.DocumentName = "v1.0";
             });
 
             app.UseRouting();
