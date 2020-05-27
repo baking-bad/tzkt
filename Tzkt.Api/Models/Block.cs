a2aa73751321a17ebe0dc8e12cebdb06a23d683a
dimanmir <demonmir@inbox.ru>
2020-05-27 12:38:05 +0300

    Voting and root models added

--- Tzkt.Api/Models/Block.cs
+++ Tzkt.Api/Models/Block.cs
@@ -7,40 +7,119 @@ namespace Tzkt.Api.Models
 {
     public class Block
     {
+        /// <summary>
+        /// The height of the block from the genesis block
+        /// </summary>
         public int Level { get; set; }
         
+        /// <summary>
+        /// Block hash
+        /// </summary>
         public string Hash { get; set; }
         
+        /// <summary>
+        /// The datetime at which the block is claimed to have been created (ISO 8601, e.g. `2020-02-20T02:40:57Z`)
+        /// </summary>
         public DateTime Timestamp { get; set; }
         
+        /// <summary>
+        /// Protocol code, representing a number of protocol changes since genesis (mod 256, but `-1` for the genesis block)
+        /// </summary>
         public int Proto { get; set; }
         
+        /// <summary>
+        /// The position in the priority list of delegates at which the block was baked
+        /// </summary>
         public int Priority { get; set; }
         
+        /// <summary>
+        /// Number of endorsements, confirmed the block
+        /// </summary>
         public int Validations { get; set; }
 
+        /// <summary>
+        /// Reward of the baker for producing the block (micro tez)
+        /// </summary>
         public long Reward { get; set; }
 
+        /// <summary>
+        /// Total fee paid by all operations, included in the block
+        /// </summary>
         public long Fees { get; set; }
 
+        /// <summary>
+        /// Status of the seed nonce revelation
+        /// `true` - seed nonce revealed
+        /// `false` - there's no `seed_nonce_hash` in the block or seed nonce revelation has missed
+        /// </summary>
         public bool NonceRevealed { get; set; }
 
+        /// <summary>
+        /// Information about a delegate (baker), produced the block
+        /// </summary>
         public Alias Baker { get; set; }
 
         #region operations
+        /// <summary>
+        /// List of endorsement (is operation, which specifies the head of the chain as seen by the endorser of a given slot)
+        /// operations, included in the block
+        /// </summary>
         public IEnumerable<EndorsementOperation> Endorsements { get; set; }
 
+        /// <summary>
+        /// List of proposal (is used by bakers (delegates) to submit and/or upvote proposals to amend the protocol)
+        /// operations, included in the block
+        /// </summary>
         public IEnumerable<ProposalOperation> Proposals { get; set; }
+        
+        /// <summary>
+        /// List of ballot (is used to vote for a proposal in a given voting cycle) operations, included in the block
+        /// </summary>
         public IEnumerable<BallotOperation> Ballots { get; set; }
 
+        /// <summary>
+        /// List of activation (is used to activate accounts that were recommended allocations of
+        /// tezos tokens for donations to the Tezos Foundation’s fundraiser) operations, included in the block
+        /// </summary>
         public IEnumerable<ActivationOperation> Activations { get; set; }
+        
+        /// <summary>
+        /// List of double baking evidence (is used by bakers to provide evidence of double baking (baking two different
+        /// blocks at the same height) by a baker) operations, included in the block
+        /// </summary>
         public IEnumerable<DoubleBakingOperation> DoubleBaking { get; set; }
+        
+        /// <summary>
+        /// List of double endorsement evidence (is used by bakers to provide evidence of double endorsement
+        /// (endorsing two different blocks at the same block height) by a baker) operations, included in the block
+        /// </summary>
         public IEnumerable<DoubleEndorsingOperation> DoubleEndorsing { get; set; }
+        
+        /// <summary>
+        /// List of nonce revelation (are used by the blockchain to create randomness) operations, included in the block
+        /// </summary>
         public IEnumerable<NonceRevelationOperation> NonceRevelations { get; set; }
 
+        /// <summary>
+        /// List of delegation (is used to delegate funds to a delegate (an implicit account registered as a baker))
+        /// operations, included in the block
+        /// </summary>
         public IEnumerable<DelegationOperation> Delegations { get; set; }
+        
+        /// <summary>
+        /// List of origination (deployment / contract creation ) operations, included in the block
+        /// </summary>
         public IEnumerable<OriginationOperation> Originations { get; set; }
+        
+        /// <summary>
+        /// List of transaction (is a standard operation used to transfer tezos tokens to an account)
+        /// operations, included in the block
+        /// </summary>
         public IEnumerable<TransactionOperation> Transactions { get; set; }
+        
+        /// <summary>
+        /// List of reveal (is used to reveal the public key associated with an account) operations, included in the block
+        /// </summary>
         public IEnumerable<RevealOperation> Reveals { get; set; }
         #endregion
     }
--- Tzkt.Api/Models/PeriodInfo.cs
+++ Tzkt.Api/Models/PeriodInfo.cs
@@ -7,12 +7,30 @@ namespace Tzkt.Api.Models
 {
     public class PeriodInfo
     {
+        /// <summary>
+        /// Protocol-level voting period counter
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// Kind of the voting period
+        /// `proposal` - delegates can submit protocol amendment proposals using the proposal operation
+        /// `exploration` -  bakers (delegates) may vote on the top-ranked proposal from the previous Proposal Period using the ballot operation
+        /// `testing` - If the proposal is approved in the Exploration Period, the Testing Period begins with a testnet
+        /// fork that runs in parallel to the main network for 48 hours to test a correct migration of the context
+        /// `promotion` - delegates can cast one vote to promote or not the tested proposal using the ballot operation
+        /// Learn more: https://tezos.gitlab.io/whitedoc/voting.html
+        /// </summary>
         public string Kind { get; set; }
 
+        /// <summary>
+        /// The height of the block in which the period starts
+        /// </summary>
         public int StartLevel { get; set; }
 
+        /// <summary>
+        /// The height of the block in which the period ends
+        /// </summary>
         public int EndLevel { get; set; }
     }
 }
