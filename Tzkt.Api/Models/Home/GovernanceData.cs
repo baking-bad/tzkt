63aba31eded78a21b5e90826f0e4a2b5dc7c0e65
dmir <demonmir@inbox.ru>
2021-07-14 14:43:09 +0300

    Added EpochStartTime, changed Quorum to BallotsQuorum

--- Tzkt.Api/Models/Home/GovernanceData.cs
+++ Tzkt.Api/Models/Home/GovernanceData.cs
@@ -10,12 +10,13 @@ namespace Tzkt.Api.Models
         
         public string Period { get; set; }
         public DateTime PeriodEndTime { get; set; }
+        public DateTime EpochStartTime { get; set; }
         public DateTime EpochEndTime { get; set; }
 
         public List<ProposalData> Proposals { get; set; }
         public double? UpvotesQuorum { get; set; }
         
-        public double? Quorum { get; set; }
+        public double? BallotsQuorum { get; set; }
         public double? Participation { get; set; }
 
         public double? Supermajority { get; set; }
