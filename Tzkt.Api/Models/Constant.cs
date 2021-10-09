d587feeda4cce676f38abd90264b03e547b487d6
257Byte <257Byte@gmail.com>
2021-10-09 17:19:20 +0300

    Add API endpoints for global constants

---
+++ Tzkt.Api/Models/Constant.cs
@@ -0,0 +1,48 @@
+﻿using Netezos.Encoding;
+using System;
+
+namespace Tzkt.Api.Models
+{
+    public class Constant
+    {
+        /// <summary>
+        /// Global address (expression hash)
+        /// </summary>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// Constant micheline expression
+        /// </summary>
+        public IMicheline Value { get; set; }
+
+        /// <summary>
+        /// Constant size in bytes
+        /// </summary>
+        public int Size { get; set; }
+
+        /// <summary>
+        /// Number of contracts referencing this constant
+        /// </summary>
+        public int Refs { get; set; }
+
+        /// <summary>
+        /// Account registered this constant
+        /// </summary>
+        public Alias Creator { get; set; }
+
+        /// <summary>
+        /// Level of the first block baked with this software
+        /// </summary>
+        public int CreationLevel { get; set; }
+
+        /// <summary>
+        /// Datetime of the first block baked with this software
+        /// </summary>
+        public DateTime CreationTime { get; set; }
+
+        /// <summary>
+        /// Offchain metadata
+        /// </summary>
+        public RawJson Metadata { get; set; }
+    }
+}
---
+++ Tzkt.Api/Validation/ExpressionHashAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace System.ComponentModel.DataAnnotations
+{
+    public sealed class ExpressionHashAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            return value != null && !Regex.IsMatch((string)value, "^expr[0-9A-Za-z]{50}$")
+                ? new ValidationResult("Invalid expression hash.")
+                : ValidationResult.Success;
+        }
+    }
+}
