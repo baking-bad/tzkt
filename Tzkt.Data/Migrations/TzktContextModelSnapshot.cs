c759bc232526defd2cd7ece2b74f733da975816c
257Byte <257Byte@gmail.com>
2021-11-06 01:39:26 +0300

    Reorder migrations

--- Tzkt.Data/Migrations/TzktContextModelSnapshot.cs
+++ Tzkt.Data/Migrations/TzktContextModelSnapshot.cs
@@ -16,7 +16,7 @@ namespace Tzkt.Data.Migrations
 #pragma warning disable 612, 618
             modelBuilder
                 .HasAnnotation("Relational:MaxIdentifierLength", 63)
-                .HasAnnotation("ProductVersion", "5.0.8")
+                .HasAnnotation("ProductVersion", "5.0.10")
                 .HasAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
 
             modelBuilder.Entity("Tzkt.Data.Models.Account", b =>
