288b17441ce4fff4b78a42bf89a1b850a9e8d183
257Byte <257byte@gmail.com>
2022-01-13 16:20:56 +0300

    Fix binding JsonParameter with root path

--- Tzkt.Api/Parameters/Binders/JsonBinder.cs
+++ Tzkt.Api/Parameters/Binders/JsonBinder.cs
@@ -13,104 +13,115 @@ namespace Tzkt.Api
             var model = ctx.ModelName;
             JsonParameter res = null;
 
-            foreach (var key in ctx.HttpContext.Request.Query.Keys.Where(x => x == model || x.StartsWith($"{model}.")))
+            foreach (var key in ctx.HttpContext.Request.Query.Keys)
             {
-                if (!JsonPath.TryParse(key[Math.Min(model.Length + 1, key.Length)..], out var path))
+                if (key == model)
                 {
-                    ctx.ModelState.AddModelError(key,
-                        $"Path contains invalid item: {path.First(x => x.Type == JsonPathType.None).Value}");
-                    return Task.CompletedTask;
+                    if (!ctx.TryGetJson(key, out var val))
+                        return Task.CompletedTask;
+                    res ??= new();
+                    res.Eq ??= new();
+                    res.Eq.Add((Array.Empty<JsonPath>(), val));
                 }
-                if (path.Any(x => x.Type == JsonPathType.Any) && path.Any(x => x.Type == JsonPathType.Index))
+                else if (key.StartsWith($"{model}."))
                 {
-                    ctx.ModelState.AddModelError(key,
-                        $"Mixed array access is not allowed: [{path.First(x => x.Type == JsonPathType.Index).Value}] and [*]");
-                    return Task.CompletedTask;
-                }
+                    if (!JsonPath.TryParse(key[(model.Length + 1)..], out var path))
+                    {
+                        ctx.ModelState.AddModelError(key,
+                            $"Path contains invalid item: {path.First(x => x.Type == JsonPathType.None).Value}");
+                        return Task.CompletedTask;
+                    }
+                    if (path.Any(x => x.Type == JsonPathType.Any) && path.Any(x => x.Type == JsonPathType.Index))
+                    {
+                        ctx.ModelState.AddModelError(key,
+                            $"Mixed array access is not allowed: [{path.First(x => x.Type == JsonPathType.Index).Value}] and [*]");
+                        return Task.CompletedTask;
+                    }
 
-                res ??= new JsonParameter();
-                switch (path[^1].Value)
-                {
-                    case "eq":
-                        if (!ctx.TryGetJson(key, out var eq))
-                            return Task.CompletedTask;
-                        res.Eq ??= new();
-                        res.Eq.Add((path[..^1], eq));
-                        break;
-                    case "ne":
-                        if (!ctx.TryGetJson(key, out var ne))
-                            return Task.CompletedTask;
-                        res.Ne ??= new();
-                        res.Ne.Add((path[..^1], ne));
-                        break;
-                    case "gt":
-                        if (HasWildcard(ctx, key, path) || !ctx.TryGetString(key, out var gt))
-                            return Task.CompletedTask;
-                        res.Gt ??= new();
-                        res.Gt.Add((path[..^1], gt));
-                        break;
-                    case "ge":
-                        if (HasWildcard(ctx, key, path) || !ctx.TryGetString(key, out var ge))
-                            return Task.CompletedTask;
-                        res.Ge ??= new();
-                        res.Ge.Add((path[..^1], ge));
-                        break;
-                    case "lt":
-                        if (HasWildcard(ctx, key, path) || !ctx.TryGetString(key, out var lt))
-                            return Task.CompletedTask;
-                        res.Lt ??= new();
-                        res.Lt.Add((path[..^1], lt));
-                        break;
-                    case "le":
-                        if (HasWildcard(ctx, key, path) || !ctx.TryGetString(key, out var le))
-                            return Task.CompletedTask;
-                        res.Le ??= new();
-                        res.Le.Add((path[..^1], le));
-                        break;
-                    case "as":
-                        if (HasWildcard(ctx, key, path) || !ctx.TryGetString(key, out var @as))
-                            return Task.CompletedTask;
-                        res.As ??= new();
-                        res.As.Add((path[..^1], @as
-                            .Replace("%", "\\%")
-                            .Replace("\\*", "ъуъ")
-                            .Replace("*", "%")
-                            .Replace("ъуъ", "*")));
-                        break;
-                    case "un":
-                        if (HasWildcard(ctx, key, path) || !ctx.TryGetString(key, out var un))
-                            return Task.CompletedTask;
-                        res.Un ??= new();
-                        res.Un.Add((path[..^1], un
-                            .Replace("%", "\\%")
-                            .Replace("\\*", "ъуъ")
-                            .Replace("*", "%")
-                            .Replace("ъуъ", "*")));
-                        break;
-                    case "in":
-                        if (!ctx.TryGetJsonArray(key, out var @in))
-                            return Task.CompletedTask;
-                        res.In ??= new();
-                        res.In.Add((path[..^1], @in));
-                        break;
-                    case "ni":
-                        if (!ctx.TryGetJsonArray(key, out var ni))
-                            return Task.CompletedTask;
-                        res.Ni ??= new();
-                        res.Ni.Add((path[..^1], ni));
-                        break;
-                    case "null":
-                        if (HasWildcard(ctx, key, path) || !ctx.TryGetBool(key, out var isNull))
-                            return Task.CompletedTask;
-                        res.Null ??= new();
-                        res.Null.Add((path[..^1], (bool)isNull));
-                        break;
-                    default:
-                        if (!ctx.TryGetJson(key, out var val))
-                            return Task.CompletedTask;
-                        res.Eq ??= new();
-                        res.Eq.Add((path, val));
-                        break;
+                    res ??= new();
+                    switch (path[^1].Value)
+                    {
+                        case "eq":
+                            if (!ctx.TryGetJson(key, out var eq))
+                                return Task.CompletedTask;
+                            res.Eq ??= new();
+                            res.Eq.Add((path[..^1], eq));
+                            break;
+                        case "ne":
+                            if (!ctx.TryGetJson(key, out var ne))
+                                return Task.CompletedTask;
+                            res.Ne ??= new();
+                            res.Ne.Add((path[..^1], ne));
+                            break;
+                        case "gt":
+                            if (HasWildcard(ctx, key, path) || !ctx.TryGetString(key, out var gt))
+                                return Task.CompletedTask;
+                            res.Gt ??= new();
+                            res.Gt.Add((path[..^1], gt));
+                            break;
+                        case "ge":
+                            if (HasWildcard(ctx, key, path) || !ctx.TryGetString(key, out var ge))
+                                return Task.CompletedTask;
+                            res.Ge ??= new();
+                            res.Ge.Add((path[..^1], ge));
+                            break;
+                        case "lt":
+                            if (HasWildcard(ctx, key, path) || !ctx.TryGetString(key, out var lt))
+                                return Task.CompletedTask;
+                            res.Lt ??= new();
+                            res.Lt.Add((path[..^1], lt));
+                            break;
+                        case "le":
+                            if (HasWildcard(ctx, key, path) || !ctx.TryGetString(key, out var le))
+                                return Task.CompletedTask;
+                            res.Le ??= new();
+                            res.Le.Add((path[..^1], le));
+                            break;
+                        case "as":
+                            if (HasWildcard(ctx, key, path) || !ctx.TryGetString(key, out var @as))
+                                return Task.CompletedTask;
+                            res.As ??= new();
+                            res.As.Add((path[..^1], @as
+                                .Replace("%", "\\%")
+                                .Replace("\\*", "ъуъ")
+                                .Replace("*", "%")
+                                .Replace("ъуъ", "*")));
+                            break;
+                        case "un":
+                            if (HasWildcard(ctx, key, path) || !ctx.TryGetString(key, out var un))
+                                return Task.CompletedTask;
+                            res.Un ??= new();
+                            res.Un.Add((path[..^1], un
+                                .Replace("%", "\\%")
+                                .Replace("\\*", "ъуъ")
+                                .Replace("*", "%")
+                                .Replace("ъуъ", "*")));
+                            break;
+                        case "in":
+                            if (!ctx.TryGetJsonArray(key, out var @in))
+                                return Task.CompletedTask;
+                            res.In ??= new();
+                            res.In.Add((path[..^1], @in));
+                            break;
+                        case "ni":
+                            if (!ctx.TryGetJsonArray(key, out var ni))
+                                return Task.CompletedTask;
+                            res.Ni ??= new();
+                            res.Ni.Add((path[..^1], ni));
+                            break;
+                        case "null":
+                            if (HasWildcard(ctx, key, path) || !ctx.TryGetBool(key, out var isNull))
+                                return Task.CompletedTask;
+                            res.Null ??= new();
+                            res.Null.Add((path[..^1], (bool)isNull));
+                            break;
+                        default:
+                            if (!ctx.TryGetJson(key, out var val))
+                                return Task.CompletedTask;
+                            res.Eq ??= new();
+                            res.Eq.Add((path, val));
+                            break;
+                    }
                 }
             }
 
