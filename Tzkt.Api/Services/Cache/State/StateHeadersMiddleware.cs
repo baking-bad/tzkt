a332100a6e3b59281ff1b0337a82aa46e0aee344
257Byte <257Byte@gmail.com>
2022-01-27 01:08:42 +0300

    Inject tzkt state to http headers

---
+++ Tzkt.Api/Services/Cache/State/StateHeadersMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Tzkt.Api.Services.Cache
+{
+    public class StateHeadersMiddleware
+    {
+        public const string TZKT_LEVEL = "Tzkt-Level";
+        public const string TZKT_KNOWN_LEVEL = "Tzkt-Known-Level";
+        public const string TZKT_SYNCED_AT = "Tzkt-Synced-At";
+
+        readonly RequestDelegate Next;
+        public StateHeadersMiddleware(RequestDelegate next) => Next = next;
+
+        public Task InvokeAsync(HttpContext context, StateCache stateCache)
+        {
+            var state = stateCache.Current;
+            context.Response.Headers.Add(TZKT_LEVEL, state.Level.ToString());
+            context.Response.Headers.Add(TZKT_KNOWN_LEVEL, state.KnownHead.ToString());
+            context.Response.Headers.Add(TZKT_SYNCED_AT, state.LastSync.ToString("yyyy-MM-ddTHH:mm:ssZ"));
+            return Next(context);
+        }
+    }
+}
