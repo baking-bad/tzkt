54159249ac411c46fae60b9cf649fbd8596a3312
257Byte <257Byte@gmail.com>
2022-03-06 14:11:30 +0300

    Rework diagnostics

--- Tzkt.Sync/Protocols/Handlers/Proto12/Diagnostics/Diagnostics.cs
+++ Tzkt.Sync/Protocols/Handlers/Proto12/Diagnostics/Diagnostics.cs
@@ -1,7 +1,40 @@
-﻿namespace Tzkt.Sync.Protocols.Proto12
+﻿using System;
+using System.Threading.Tasks;
+using Tzkt.Data.Models;
+
+namespace Tzkt.Sync.Protocols.Proto12
 {
-    class Diagnostics : Proto10.Diagnostics
+    class Diagnostics : Proto5.Diagnostics
     {
         public Diagnostics(ProtocolHandler handler) : base(handler) { }
+
+        protected override async Task TestDelegate(int level, Data.Models.Delegate delegat, Protocol proto)
+        {
+            var remote = await Rpc.GetDelegateAsync(level, delegat.Address);
+
+            if (remote.RequiredInt64("full_balance") != delegat.Balance)
+                throw new Exception($"Diagnostics failed: wrong balance {delegat.Address}");
+
+            if (remote.RequiredInt64("frozen_deposits") != delegat.FrozenDeposit)
+                throw new Exception($"Diagnostics failed: wrong frozen deposits {delegat.Address}");
+
+            if (remote.RequiredInt64("staking_balance") != delegat.StakingBalance)
+                throw new Exception($"Diagnostics failed: wrong staking balance {delegat.Address}");
+
+            if (remote.RequiredInt64("delegated_balance") != delegat.DelegatedBalance)
+                throw new Exception($"Diagnostics failed: wrong delegated balance {delegat.Address}");
+
+            if (remote.RequiredBool("deactivated") != !delegat.Staked)
+                throw new Exception($"Diagnostics failed: wrong deactivation state {delegat.Address}");
+
+            var deactivationCycle = (delegat.DeactivationLevel - 1) >= proto.FirstLevel
+                ? proto.GetCycle(delegat.DeactivationLevel - 1)
+                : (await Cache.Blocks.GetAsync(delegat.DeactivationLevel - 1)).Cycle;
+
+            if (remote.RequiredInt32("grace_period") != deactivationCycle)
+                throw new Exception($"Diagnostics failed: wrong grace period {delegat.Address}");
+
+            TestDelegatorsCount(remote, delegat);
+        }
     }
 }
