bb6adbd4771b74186338db49238520a2f9dc1b9f
dmir <demonmir@inbox.ru>
2022-03-15 18:47:02 +0300

    Tests for accounts, blocks and operations

--- Tzkt.Api.Tests/Api/TestBlocksQueries.cs
+++ Tzkt.Api.Tests/Api/TestBlocksQueries.cs
@@ -16,11 +16,45 @@ namespace Tzkt.Api.Tests.Api
         }
 
         [Fact]
-        public async Task TestBlockHash()
+        public async Task TestBlocksCount()
         {
             var res = await Client.GetJsonAsync("/v1/blocks/count");
 
             Assert.True(res is DJsonValue);
+        }
+
+        [Fact]
+        public async Task TestBlocks()
+        {
+            var res = await Client.GetJsonAsync("/v1/blocks");
+
+            Assert.True(res is DJsonArray);
+        }
+        
+        //TODO Consider adding block by hash
+        
+        [Fact]
+        public async Task TestBlockByLevel()
+        {
+            var res = await Client.GetJsonAsync("/v1/blocks/10");
+
+            Assert.True(res is DJsonObject);
+        }
+        
+        [Fact]
+        public async Task TestBlockOperations()
+        {
+            var res = await Client.GetJsonAsync("/v1/blocks/10?operations=true");
+
+            Assert.True(res is DJsonObject);
+        }
+        
+        [Fact]
+        public async Task TestBlockQuotes()
+        {
+            var res = await Client.GetJsonAsync("/v1/blocks/10?quote=usd");
+
+            Assert.True(res is DJsonObject);
         }
     }
 }
