68bdefbf1ecdeac54ac53dd366c68d7453abbd3f
dmir <demonmir@inbox.ru>
2022-03-15 17:19:10 +0300

    Api endpoints tests initiated.

---
+++ Tzkt.Api.Tests/Api/SettingsFixture.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using Dynamic.Json;
+using Netezos.Rpc;
+
+namespace Tzkt.Api.Tests.Api
+{
+    public class SettingsFixture : IDisposable
+    {
+        static readonly object Crit = new object();
+
+        public HttpClient Client { get; }
+
+        public SettingsFixture()
+        {
+            lock (Crit)
+            {
+                var settings = DJson.Read("../../../Api/settings.json");
+
+                Client = new HttpClient()
+                {
+                    BaseAddress = new Uri(settings.Url) 
+                };
+            }
+        }
+
+        public void Dispose() => Client.Dispose();
+    }
+}
---
+++ Tzkt.Api.Tests/Api/TestBlocksQueries.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Dynamic.Json;
+using Dynamic.Json.Extensions;
+using Xunit;
+
+namespace Tzkt.Api.Tests.Api
+{
+    public class TestBlocksQueries : IClassFixture<SettingsFixture>
+    {
+        readonly HttpClient Client;
+
+        public TestBlocksQueries(SettingsFixture settings)
+        {
+            Client = settings.Client;
+        }
+
+        [Fact]
+        public async Task TestBlockHash()
+        {
+            var res = await Client.GetJsonAsync("/v1/blocks/count");
+
+            Assert.True(res is DJsonValue);
+        }
+    }
+}
