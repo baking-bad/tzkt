96fbc91dd9caf4f33edec53079ea39ee8974716d
Michael Zaikin <mz@baking-bad.org>
2022-03-21 18:54:19 +0300

    Allow multiple DipDup token metadata sources (#87)

--- Tzkt.Sync/Services/TokenMetadata/TokenMetadataConfig.cs
+++ Tzkt.Sync/Services/TokenMetadata/TokenMetadataConfig.cs
@@ -6,26 +6,20 @@ namespace Tzkt.Sync.Services
 {
     public class TokenMetadataConfig
     {
-        public bool Enabled { get; set; } = false;
-        public string DipDupUrl { get; set; } = "https://metadata.dipdup.net/v1/graphql";
-        public string Network { get; set; } = "mainnet";
+        public bool Enabled { get; set; } = false;       
         public int BatchSize { get; set; } = 100;
         public int PeriodSec { get; set; } = 60;
+        public List<DipDupConfig> DipDup { get; set; }
+        public List<TokenMetadataItem> OverriddenMetadata { get; set; }
+    }
 
-        public List<TokenMetadataItem> OverriddenMetadata { get; set; } = new()
-        {
-            new("KT1PWx2mnDueood7fEmfbBDKx1D9BAnnXitn", @"{""name"":""tzBTC"",""symbol"":""tzBTC"",""decimals"":""8""}"),
-            new("KT1VYsVfmobT7rsMVivvZ4J8i3bPiqz12NaH", @"{""name"":""wXTZ"",""symbol"":""wXTZ"",""decimals"":""6""}"),
-            new("KT1LN4LPSqTMS7Sd2CJw4bbDGRkMv2t68Fy9", @"{""name"":""USDtez"",""symbol"":""USDtz"",""decimals"":""6""}"),
-            new("KT19at7rQUvyjxnZ2fBv7D9zc8rkyG7gAoU8", @"{""name"":""ETHtez"",""symbol"":""ETHtz"",""decimals"":""18""}"),
-            new("KT1REEb5VxWRjcHm5GzDMwErMmNFftsE5Gpf", @"{""name"":""Stably USD"",""symbol"":""USDS"",""decimals"":""6""}"),
-            new("KT1AEfeckNbdEYwaMKkytBwPJPycz7jdSGea", @"{""name"":""STKR"",""symbol"":""STKR"",""decimals"":""18""}"),
-            new("KT1AafHA1C1vk959wvHWBispY9Y2f3fxBUUo", @"{""name"":""LB Token"",""symbol"":""LBT"",""decimals"":""0""}"),
-            new("KT1K9gCRgaLRFKTErYt1wVxA3Frb9FjasjTV", @"{""name"":""Kolibri USD"",""symbol"":""kUSD"",""decimals"":""18""}"),
-            new("KT1AxaBxkFLCUi3f8rdDAAxBKHfzY8LfKDRA", @"{""name"":""Quipuswap Liquidating kUSD"",""symbol"":""QLkUSD"",""decimals"":""36""}"),
-            new("KT1AFA2mwNUMNd4SsujE1YYp29vd8BZejyKW", @"{""name"":""Hic et nunc DAO"",""symbol"":""hDAO"",""decimals"":""6""}"),
-            new("KT1LqEyTQxD2Dsdkk4LME5YGcBqazAwXrg4t", @"{""name"":""Werecoin"",""symbol"":""WRC"",""decimals"":""6""}")
-        };
+    public class DipDupConfig
+    {
+        public string Url { get; set; }
+        public string MetadataTable { get; set; } = "dipdup_token_metadata";
+        public string HeadStatusTable { get; set; } = "dipdup_head";
+        public string Network { get; set; } = "mainnet";
+        public int SelectLimit { get; set; } = 10_000;
     }
 
     public class TokenMetadataItem
