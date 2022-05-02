2cc3d3508efa7ea163d4cbdc9c31169f977ed449
257Byte <257byte@gmail.com>
2022-05-02 16:31:42 +0300

    Add filtering ballot operations by vote, close #97

---
+++ Tzkt.Api/Parameters/VoteParameter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using NJsonSchema.Annotations;
+
+namespace Tzkt.Api
+{
+    [ModelBinder(BinderType = typeof(VoteBinder))]
+    [JsonSchemaExtensionData("x-tzkt-extension", "query-parameter")]
+    [JsonSchemaExtensionData("x-tzkt-query-parameter", "yay,nay,pass")]
+    public class VoteParameter
+    {
+        /// <summary>
+        /// **Equal** filter mode (optional, i.e. `param.eq=123` is the same as `param=123`). \
+        /// Specify a value to get items where the specified field is equal to the specified value.
+        /// 
+        /// Example: `?vote=yay`.
+        /// </summary>
+        [JsonSchemaType(typeof(string))]
+        public int? Eq { get; set; }
+
+        /// <summary>
+        /// **Not equal** filter mode. \
+        /// Specify a value to get items where the specified field is not equal to the specified value.
+        /// 
+        /// Example: `?vote.ne=pass`.
+        /// </summary>
+        [JsonSchemaType(typeof(string))]
+        public int? Ne { get; set; }
+
+        /// <summary>
+        /// **In list** (any of) filter mode. \
+        /// Specify a comma-separated list of values to get items where the specified field is equal to one of the specified values.
+        /// 
+        /// Example: `?vote.in=yay,nay`.
+        /// </summary>
+        [JsonSchemaType(typeof(List<string>))]
+        public List<int> In { get; set; }
+
+        /// <summary>
+        /// **Not in list** (none of) filter mode. \
+        /// Specify a comma-separated list of values to get items where the specified field is not equal to all the specified values.
+        /// 
+        /// Example: `?vote.ni=yay,nay`.
+        /// </summary>
+        [JsonSchemaType(typeof(List<string>))]
+        public List<int> Ni { get; set; }
+    }
+}
--- Tzkt.Api/Repositories/Enums/Votes.cs
+++ Tzkt.Api/Repositories/Enums/Votes.cs
@@ -9,6 +9,18 @@ namespace Tzkt.Api
         public const string Nay = "nay";
         public const string Pass = "pass";
 
+        public static bool TryParse(string value, out int res)
+        {
+            res = value switch
+            {
+                Yay => (int)Vote.Yay,
+                Nay => (int)Vote.Nay,
+                Pass => (int)Vote.Pass,
+                _ => -1
+            };
+            return res != -1;
+        }
+
         public static string ToString(int value) => value switch
         {
             (int)Vote.Yay => Yay,
