8bc602098951346fdb3b81dd3c9facea72fc8ce7
257Byte <257byte@gmail.com>
2022-05-09 00:36:12 +0300

    Use lazy_storage_diff instead of deprecated big_map_diff

--- Tzkt.Sync/Protocols/Handlers/Proto13/Commits/Operations/TransactionsCommit.cs
+++ Tzkt.Sync/Protocols/Handlers/Proto13/Commits/Operations/TransactionsCommit.cs
@@ -1,7 +1,18 @@
-﻿namespace Tzkt.Sync.Protocols.Proto13
+﻿using System.Collections.Generic;
+using System.Text.Json;
+using Tzkt.Data.Models;
+
+namespace Tzkt.Sync.Protocols.Proto13
 {
     class TransactionsCommit : Proto5.TransactionsCommit
     {
         public TransactionsCommit(ProtocolHandler protocol) : base(protocol) { }
+
+        protected override IEnumerable<BigMapDiff> ParseBigMapDiffs(TransactionOperation transaction, JsonElement result)
+        {
+            return result.TryGetProperty("lazy_storage_diff", out var diffs)
+                ? BigMapDiff.ParseLazyStorage(diffs)
+                : null;
+        }
     }
 }
