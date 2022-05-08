fb7696f2a76c6a1d317ab2c301d1c54851815984
257Byte <257byte@gmail.com>
2022-05-09 00:36:12 +0300

    Use lazy_storage_diff instead of deprecated big_map_diff

--- Tzkt.Sync/Protocols/Handlers/Proto13/Commits/Operations/OriginationsCommit.cs
+++ Tzkt.Sync/Protocols/Handlers/Proto13/Commits/Operations/OriginationsCommit.cs
@@ -1,7 +1,19 @@
-﻿namespace Tzkt.Sync.Protocols.Proto13
+﻿using System.Collections.Generic;
+using System.Text.Json;
+using Netezos.Encoding;
+using Tzkt.Data.Models;
+
+namespace Tzkt.Sync.Protocols.Proto13
 {
     class OriginationsCommit : Proto5.OriginationsCommit
     {
         public OriginationsCommit(ProtocolHandler protocol) : base(protocol) { }
+
+        protected override IEnumerable<BigMapDiff> ParseBigMapDiffs(OriginationOperation origination, JsonElement result, MichelineArray code, IMicheline storage)
+        {
+            return result.TryGetProperty("lazy_storage_diff", out var diffs)
+                ? BigMapDiff.ParseLazyStorage(diffs)
+                : null;
+        }
     }
 }
--- Tzkt.Sync/Protocols/Helpers/BigMapDiff.cs
+++ Tzkt.Sync/Protocols/Helpers/BigMapDiff.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Collections.Generic;
+using System.Text.Json;
 using Netezos.Encoding;
 
 namespace Tzkt.Sync.Protocols
@@ -59,6 +60,79 @@ namespace Tzkt.Sync.Protocols
                 },
                 _ => throw new ValidationException($"Unknown big_map_diff action")
             };
+        }
+
+        public static IEnumerable<BigMapDiff> ParseLazyStorage(JsonElement diffs)
+        {
+            foreach (var diff in diffs.RequiredArray().EnumerateArray())
+            {
+                if (diff.RequiredString("kind") == "big_map")
+                {
+                    var content = diff.Required("diff");
+                    switch (content.RequiredString("action"))
+                    {
+                        case "update":
+                            foreach (var update in content.RequiredArray("updates").EnumerateArray())
+                            {
+                                yield return new UpdateDiff
+                                {
+                                    Ptr = diff.RequiredInt32("id"),
+                                    KeyHash = update.RequiredString("key_hash"),
+                                    Key = Micheline.FromJson(update.Required("key")),
+                                    Value = update.TryGetProperty("value", out var v)
+                                        ? Micheline.FromJson(v)
+                                        : null
+                                };
+                            }
+                            break;
+                        case "remove":
+                            yield return new RemoveDiff
+                            {
+                                Ptr = diff.RequiredInt32("id")
+                            };
+                            break;
+                        case "copy":
+                            yield return new CopyDiff
+                            {
+                                Ptr = diff.RequiredInt32("id"),
+                                SourcePtr = content.RequiredInt32("source")
+                            };
+                            foreach (var update in content.RequiredArray("updates").EnumerateArray())
+                            {
+                                yield return new UpdateDiff
+                                {
+                                    Ptr = diff.RequiredInt32("id"),
+                                    KeyHash = update.RequiredString("key_hash"),
+                                    Key = Micheline.FromJson(update.Required("key")),
+                                    Value = update.TryGetProperty("value", out var v)
+                                        ? Micheline.FromJson(v)
+                                        : null
+                                };
+                            }
+                            break;
+                        case "alloc":
+                            yield return new AllocDiff
+                            {
+                                Ptr = diff.RequiredInt32("id")
+                            };
+                            foreach (var update in content.RequiredArray("updates").EnumerateArray())
+                            {
+                                yield return new UpdateDiff
+                                {
+                                    Ptr = diff.RequiredInt32("id"),
+                                    KeyHash = update.RequiredString("key_hash"),
+                                    Key = Micheline.FromJson(update.Required("key")),
+                                    Value = update.TryGetProperty("value", out var v)
+                                        ? Micheline.FromJson(v)
+                                        : null
+                                };
+                            }
+                            break;
+                        default:
+                            throw new ValidationException("Unknown bigmap diff action");
+                    }
+                }
+            }
         }
         #endregion
 
