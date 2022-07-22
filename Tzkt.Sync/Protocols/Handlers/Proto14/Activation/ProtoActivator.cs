11a6bec1930ab5a2d3ad2f1872590b798141a665
MS <mvstrebkov@gmail.com>
2022-07-22 14:32:05 +0300

    Implement migration and change account bootstrap logic

--- Tzkt.Sync/Protocols/Handlers/Proto14/Activation/ProtoActivator.cs
+++ Tzkt.Sync/Protocols/Handlers/Proto14/Activation/ProtoActivator.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json.Linq;
 using Tzkt.Data.Models;
 
@@ -15,9 +17,57 @@ namespace Tzkt.Sync.Protocols.Proto14
 
         protected override void UpgradeParameters(Protocol protocol, Protocol prev)
         {
+            if (Cache.AppState.GetChainId() == "NetXnHfVqm9iesp") // ghostnet
+                protocol.BlocksPerVoting = prev.BlocksPerCycle;
         }
 
-        protected override Task MigrateContext(AppState state) => Task.CompletedTask;
-        protected override Task RevertContext(AppState state) => Task.CompletedTask;
+        protected override async Task MigrateContext(AppState state)
+        {
+            var block = await Cache.Blocks.CurrentAsync();
+            var account = await Cache.Accounts.GetAsync("tz1X81bCXPtMiHu1d4UZF4GPhMPkvkp56ssb");
+
+            Db.TryAttach(account);
+            account.FirstLevel = Math.Min(account.FirstLevel, state.Level);
+            account.LastLevel = state.Level;
+            account.Balance += 3_000_000_000L;
+            account.MigrationsCount++;
+
+            block.Operations |= Operations.Migrations;
+            Db.MigrationOps.Add(new MigrationOperation
+            {
+                Id = Cache.AppState.NextOperationId(),
+                Block = block,
+                Level = block.Level,
+                Timestamp = block.Timestamp,
+                Account = account,
+                Kind = MigrationKind.ProposalInvoice,
+                BalanceChange = 3_000_000_000L
+            });
+
+            state.MigrationOpsCount++;
+
+            var stats = await Cache.Statistics.GetAsync(state.Level);
+            stats.TotalCreated += 3_000_000_000L;
+        }
+
+        protected override async Task RevertContext(AppState state)
+        {
+            var block = await Cache.Blocks.CurrentAsync();
+
+            var invoice = await Db.MigrationOps
+                .AsNoTracking()
+                .Include(x => x.Account)
+                .FirstOrDefaultAsync(x => x.Level == block.Level && x.Kind == MigrationKind.ProposalInvoice);
+
+            Db.TryAttach(invoice.Account);
+            Cache.Accounts.Add(invoice.Account);
+
+            invoice.Account.Balance -= invoice.BalanceChange;
+            invoice.Account.MigrationsCount--;
+
+            Db.MigrationOps.Remove(invoice);
+
+            state.MigrationOpsCount--;
+        }
     }
 }
