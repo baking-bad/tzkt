13cebaed094b801fdc718637ce811f8376554c8d
MS <mvstrebkov@gmail.com>
2022-08-18 16:11:44 +0300

    Failed reveals no longer take effect

--- Tzkt.Sync/Protocols/Handlers/Proto1/Commits/Operations/RevealsCommit.cs
+++ Tzkt.Sync/Protocols/Handlers/Proto1/Commits/Operations/RevealsCommit.cs
@@ -75,11 +75,7 @@ namespace Tzkt.Sync.Protocols.Proto1
             #endregion
 
             #region apply result
-            if (sender is User user)
-            {
-                user.PublicKey = pubKey;
-                if (user.Balance > 0) user.Revealed = true;
-            }
+            ApplyResult(reveal, pubKey);
             #endregion
 
             Proto.Manager.Set(reveal.Sender);
@@ -107,13 +103,7 @@ namespace Tzkt.Sync.Protocols.Proto1
             #endregion
 
             #region revert result
-            if (sender is User user)
-            {
-                if (sender.RevealsCount == 1)
-                    user.PublicKey = null;
-
-                user.Revealed = false;
-            }
+            RevertResult(reveal);
             #endregion
 
             #region revert operation
@@ -139,6 +129,26 @@ namespace Tzkt.Sync.Protocols.Proto1
         protected virtual int GetConsumedGas(JsonElement result)
         {
             return result.OptionalInt32("consumed_gas") ?? 0;
+        }
+
+        protected virtual void ApplyResult(RevealOperation op, string pubKey)
+        {
+            if (op.Sender is User user)
+            {
+                user.PublicKey = pubKey;
+                if (user.Balance > 0) user.Revealed = true;
+            }
+        }
+
+        protected virtual void RevertResult(RevealOperation op)
+        {
+            if (op.Sender is User user)
+            {
+                if (user.RevealsCount == 1)
+                    user.PublicKey = null;
+
+                user.Revealed = false;
+            }
         }
     }
 }
