bc5e30ba548dd377f1faa38918a69cf30513032c
MS <mvstrebkov@gmail.com>
2022-08-18 16:11:44 +0300

    Failed reveals no longer take effect

--- Tzkt.Sync/Protocols/Handlers/Proto14/Commits/Operations/RevealsCommit.cs
+++ Tzkt.Sync/Protocols/Handlers/Proto14/Commits/Operations/RevealsCommit.cs
@@ -1,4 +1,6 @@
 ﻿using System.Text.Json;
+using Tzkt.Data.Models;
+using Tzkt.Data.Models.Base;
 
 namespace Tzkt.Sync.Protocols.Proto14
 {
@@ -9,6 +11,18 @@ namespace Tzkt.Sync.Protocols.Proto14
         protected override int GetConsumedGas(JsonElement result)
         {
             return (int)(((result.OptionalInt64("consumed_milligas") ?? 0) + 999) / 1000);
+        }
+
+        protected override void ApplyResult(RevealOperation op, string pubKey)
+        {
+            if (op.Status != OperationStatus.Applied) return;
+            base.ApplyResult(op, pubKey);
+        }
+
+        protected override void RevertResult(RevealOperation op)
+        {
+            if (op.Status != OperationStatus.Applied) return;
+            base.RevertResult(op);
         }
     }
 }
