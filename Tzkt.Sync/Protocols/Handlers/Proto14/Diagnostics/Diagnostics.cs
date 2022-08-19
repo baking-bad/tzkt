e036c5d114d5f8ba6e86c96f7200fc1d90e91f50
257Byte <257byte@gmail.com>
2022-08-19 20:18:13 +0300

    Update diagnostics

--- Tzkt.Sync/Protocols/Handlers/Proto14/Diagnostics/Diagnostics.cs
+++ Tzkt.Sync/Protocols/Handlers/Proto14/Diagnostics/Diagnostics.cs
@@ -1,7 +1,13 @@
-﻿namespace Tzkt.Sync.Protocols.Proto14
+﻿using System.Text.Json;
+using Tzkt.Data.Models;
+
+namespace Tzkt.Sync.Protocols.Proto14
 {
     class Diagnostics : Proto12.Diagnostics
     {
         public Diagnostics(ProtocolHandler handler) : base(handler) { }
+
+        protected override bool CheckDelegatedBalance(JsonElement remote, Delegate delegat) =>
+            remote.RequiredInt64("delegated_balance") == delegat.DelegatedBalance;
     }
 }
