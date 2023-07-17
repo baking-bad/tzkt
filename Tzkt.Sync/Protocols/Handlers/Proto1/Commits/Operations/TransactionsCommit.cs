569663ab109f528c71dca460f11c6efd98f59ff5
Dmitry Mirgaleev <dmitry.mirgaleev@gmail.com>
2023-07-17 20:00:07 +0200

    WIP tickets

--- Tzkt.Sync/Protocols/Handlers/Proto1/Commits/Operations/TransactionsCommit.cs
+++ Tzkt.Sync/Protocols/Handlers/Proto1/Commits/Operations/TransactionsCommit.cs
@@ -695,7 +695,31 @@ namespace Tzkt.Sync.Protocols.Proto1
 
         protected virtual IEnumerable<TicketUpdate> ParseTicketUpdates(string property, JsonElement result)
         {
-            return null;
+            if (!result.TryGetProperty(property, out var ticketUpdates))
+                return null;
+
+            return ticketUpdates.RequiredArray().EnumerateArray().Select(x => new TicketUpdate
+            {
+                TicketToken = x.TryGetProperty("ticket_token", out var ticketToken)
+                    ? new TicketToken
+                    {
+                        Ticketer = ticketToken.RequiredString("ticketer"),
+                        ContentType = ticketToken.TryGetProperty("content_type", out var contentType)
+                            ? Micheline.FromJson(contentType)
+                            : null,
+                        Content = ticketToken.TryGetProperty("content", out var content)
+                            ? Micheline.FromJson(content)
+                            : null,
+                    }
+                    : null,
+                Updates = x.TryGetProperty("updates", out var updates)
+                    ? updates.RequiredArray().EnumerateArray().Select(y => new Update
+                    {
+                        Account = y.RequiredString("account"),
+                        Amount = y.RequiredString("amount")
+                    })
+                    : null
+            });
         }
     }
 }
