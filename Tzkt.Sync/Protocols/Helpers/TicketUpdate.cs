2a04e8816d936d3dee1e6824c93a49861954675c
Dmitry Mirgaleev <dmitry.mirgaleev@gmail.com>
2023-07-17 12:51:54 +0200

    WIP tickets

---
+++ Tzkt.Sync/Protocols/Helpers/TicketUpdate.cs
@@ -0,0 +1,23 @@
+﻿using Netezos.Encoding;
+
+namespace Tzkt.Sync.Protocols
+{
+    public class TicketUpdate
+    {
+        public TicketToken TicketToken { get; set; }
+        public IEnumerable<Update> Updates { get; set; }
+    }
+    
+    public class TicketToken
+    {
+        public string Ticketer { get; set; }
+        public IMicheline ContentType { get; set; }
+        public IMicheline Content { get; set; }
+    }
+    
+    public class Update
+    {
+        public string Account { get; set; }
+        public string Amount { get; set; }
+    }
+}
