cf06a8d4e9ddf82beb24aafe3b3da72c660a27d2
dmir <demonmir@inbox.ru>
2023-07-31 20:06:19 +0300

    Add cache for tickets

--- Tzkt.Sync/Protocols/Handlers/Proto17/Rpc/Rpc.cs
+++ Tzkt.Sync/Protocols/Handlers/Proto17/Rpc/Rpc.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json;
+using System.Text.RegularExpressions;
 using Tzkt.Sync.Services;
 
 namespace Tzkt.Sync.Protocols.Proto17
@@ -6,8 +7,12 @@ namespace Tzkt.Sync.Protocols.Proto17
     class Rpc : Proto12.Rpc
     {
         public Rpc(TezosNode node) : base(node) { }
-        
+
         public override Task<string> GetTicketBalance(int level, string address, string ticket)
-            => Node.PostAsync<string>($"chains/main/blocks/{level}/context/contracts/{address}/ticket_balance", ticket);
+        {
+            return Regex.IsMatch(address, "^sr1[0-9A-Za-z]{33}$") 
+                ? Node.PostAsync<string>($"chains/main/blocks/{level}/context/smart_rollups/smart_rollup/{address}/ticket_balance", ticket)
+                : Node.PostAsync<string>($"chains/main/blocks/{level}/context/contracts/{address}/ticket_balance", ticket);
+        }
     }
 }
