f68a7a0f06f3319e7cb68456181aeef58e6a0b4c
Maksim Strebkov <257byte@gmail.com>
2024-01-31 15:07:09 +0300

    Update accounts API models

--- Tzkt.Api/Services/Cache/Accounts/Models/RawUser.cs
+++ Tzkt.Api/Services/Cache/Accounts/Models/RawUser.cs
@@ -2,10 +2,17 @@
 {
     public class RawUser : RawAccount
     {
-        public bool? Activated { get; set; }
-        public string PublicKey { get; set; }
         public bool Revealed { get; set; }
+        public string PublicKey { get; set; }
+
+        public long StakedBalance { get; set; }
+        public long StakedPseudotokens { get; set; }
+        public long UnstakedBalance { get; set; }
+        public int? UnstakedBakerId { get; set; }
+
+        public bool? Activated { get; set; }
         public int RegisterConstantsCount { get; set; }
         public int SetDepositsLimitsCount { get; set; }
+        public int StakingOpsCount { get; set; }
     }
 }
