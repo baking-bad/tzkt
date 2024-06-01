3a01f549ed48673aa556534a3ecd8bb61c341126
Maksim Strebkov <257byte@gmail.com>
2024-06-01 11:28:39 +0300

    Take snapshot of deactivated bakers

--- Tzkt.Sync/Protocols/Handlers/Proto19/Commits/SnapshotBalanceCommit.cs
+++ Tzkt.Sync/Protocols/Handlers/Proto19/Commits/SnapshotBalanceCommit.cs
@@ -14,6 +14,7 @@ namespace Tzkt.Sync.Protocols.Proto19
 
             await RemoveOutdated(block, block.Protocol);
             await TakeSnapshot(block);
+            await TakeDeactivatedSnapshot(block);
         }
     }
 }
