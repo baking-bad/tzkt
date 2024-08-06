3b7082fdb7ef3bdae91dfb99fb63a741b182ac28
Sébastien Palmer <sebastien.palmer@functori.com>
2024-08-06 10:38:28 +0200

    [api] add an endpoint to get DAL commitment information by hash
    Authorize to select fields:
     - hash
     - level
     - slotIndex
     - publisher
    
    Authorize to filter by:
     - level
     - slotIndex
     - publisher
    
    Authorize to sort by:
     - level
     - slotIndex

--- Tzkt.Api/Controllers/DalController.cs
+++ Tzkt.Api/Controllers/DalController.cs
@@ -93,6 +93,34 @@ namespace Tzkt.Api.Controllers
                 }
             }
         }
+
+        /// <summary>
+        /// Get DAL commitments information by hash
+        /// </summary>
+        /// <remarks>
+        /// Returns information of the DAL commitments published with the specified hash.
+        /// </remarks>
+        /// <param name="hash">DAL commitment hash</param>
+        /// <param name="level">Filters by level</param>
+        /// <param name="slotIndex">Filters by slot-index</param>
+        /// <param name="publisher">Filters by DAL commitment publisher</param>
+        /// <param name="select">Specify comma-separated list of fields to include into response or leave it undefined to return full object. If you select single field, response will be an array of values in both `.fields` and `.values` modes.</param>
+        /// <param name="sort">Sorts DAL commitments by specified field. Supported fields: `level` (default), `slotIndex`.</param>
+        /// <param name="offset">Specifies which or how many items should be skipped</param>
+        /// <param name="limit">Maximum number of items to return</param>
+        /// <returns></returns>
+        [HttpGet("commitments/{hash}")]
+        public async Task<ActionResult<IEnumerable<DalCommitment>>> GetDalCommitmentsByHash(
+            [Required][DalCommitmentHash] string hash,
+            Int32Parameter level,
+            Int32Parameter slotIndex,
+            AccountParameter publisher,
+            SelectParameter select,
+            SortParameter sort,
+            OffsetParameter offset,
+            [Range(0, 10000)] int limit = 100)
+            =>
+            await GetDalCommitments(new DalCommitmentHashParameter { Eq = hash }, level, slotIndex, publisher, select, sort, offset, limit);
         #endregion
     }
 }
