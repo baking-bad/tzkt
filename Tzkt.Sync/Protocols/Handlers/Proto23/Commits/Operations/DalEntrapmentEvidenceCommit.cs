9e9d49333dad54de2dc9229e1196918e5382c4b0
Maksim Strebkov <257byte@gmail.com>
2025-06-12 17:12:49 +0300

    Rework dal entrapment handler

--- Tzkt.Sync/Protocols/Handlers/Proto23/Commits/Operations/DalEntrapmentEvidenceCommit.cs
+++ Tzkt.Sync/Protocols/Handlers/Proto23/Commits/Operations/DalEntrapmentEvidenceCommit.cs
@@ -1,4 +1,21 @@
-﻿namespace Tzkt.Sync.Protocols.Proto23
+﻿using System.Text.Json;
+
+namespace Tzkt.Sync.Protocols.Proto23
 {
-    class DalEntrapmentEvidenceCommit(ProtocolHandler protocol) : Proto22.DalEntrapmentEvidenceCommit(protocol) { }
+    class DalEntrapmentEvidenceCommit(ProtocolHandler protocol) : Proto22.DalEntrapmentEvidenceCommit(protocol)
+    {
+        protected override int GetTrapLevel(JsonElement content)
+        {
+            var op = content.Required("attestation").Required("operations");
+            if (!op.TryGetProperty("consensus_content", out var consensusContent))
+                consensusContent = op;
+
+            return consensusContent.RequiredInt32("level");
+        }
+
+        protected override int GetConsensusSlot(JsonElement content)
+        {
+            return content.RequiredInt32("consensus_slot");
+        }
+    }
 }
