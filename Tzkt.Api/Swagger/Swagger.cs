7bcc9ae16a294d84970f4a26027e015bb94c0378
Maksim Strebkov <257byte@gmail.com>
2025-06-15 18:46:14 +0300

    v1.15.1

--- Tzkt.Api/Swagger/Swagger.cs
+++ Tzkt.Api/Swagger/Swagger.cs
@@ -2,7 +2,7 @@
 {
     public static class Swagger
     {
-        const string Version = "1.15.0";
+        const string Version = "1.15.1";
         const string Path = "/v1/swagger.json";
 
         public static void AddOpenApiDocument(this IServiceCollection services)
