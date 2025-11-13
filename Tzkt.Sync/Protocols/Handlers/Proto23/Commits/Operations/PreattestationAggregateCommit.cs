7f9fb8ec874475eb67652ad61c6bd940137797fd
dm <dm@baking-bad.org>
2025-11-13 13:19:51 +0300

    Fix PreattestationsCommit.cs

--- Tzkt.Sync/Protocols/Handlers/Proto23/Commits/Operations/PreattestationAggregateCommit.cs
+++ Tzkt.Sync/Protocols/Handlers/Proto23/Commits/Operations/PreattestationAggregateCommit.cs
@@ -12,8 +12,7 @@ namespace Tzkt.Sync.Protocols.Proto23
             foreach (var c in content.Required("metadata").RequiredArray("committee").EnumerateArray())
             {
                 var baker = Cache.Accounts.GetExistingDelegate(c.RequiredString("delegate"));
-                var consensus = c.Required("consensus_power");
-                var slots = consensus.RequiredInt32("slots");
+                var slots = c.RequiredInt32("consensus_power");
                 res.Add((opHash, baker.Address, slots));
             }
 
--- Tzkt.Sync/Protocols/Handlers/Proto24/Commits/Operations/PreattestationsCommit.cs
+++ Tzkt.Sync/Protocols/Handlers/Proto24/Commits/Operations/PreattestationsCommit.cs
@@ -1,4 +1,15 @@
+using System.Text.Json;
+
 namespace Tzkt.Sync.Protocols.Proto24
 {
-    class PreattestationsCommit(ProtocolHandler protocol) : Proto23.PreattestationsCommit(protocol) { }
+    class PreattestationsCommit : Proto23.PreattestationsCommit
+    {
+        public PreattestationsCommit(ProtocolHandler protocol) : base(protocol) { }
+
+        protected override int GetPreattestedSlots(JsonElement metadata)
+        {
+            var consensus = metadata.Required("consensus_power");
+            return consensus.RequiredInt32("slots");
+        }
+    }
 }
