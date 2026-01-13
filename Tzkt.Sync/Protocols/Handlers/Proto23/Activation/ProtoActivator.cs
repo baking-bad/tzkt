c721b91cf6cb5ccfa233c741f5de215a3e0a72a1
Maksim Strebkov <257byte@gmail.com>
2026-01-13 15:06:21 +0300

    Fix seoul migration on shadownet

--- Tzkt.Sync/Protocols/Handlers/Proto23/Activation/ProtoActivator.cs
+++ Tzkt.Sync/Protocols/Handlers/Proto23/Activation/ProtoActivator.cs
@@ -26,6 +26,9 @@ namespace Tzkt.Sync.Protocols.Proto23
 
         protected override async Task MigrateContext(AppState state)
         {
+            var prevProto = await Cache.Protocols.GetAsync(state.Protocol);
+            var nextProto = await Cache.Protocols.GetAsync(state.NextProtocol);
+
             #region unreveal tz4
             foreach (var account in await Db.Users.Where(x => x.Revealed && x.Address.StartsWith("tz4")).ToListAsync())
             {
@@ -34,8 +37,19 @@ namespace Tzkt.Sync.Protocols.Proto23
                 account.Revealed = false;
             }
             #endregion
+
+            #region update revelation rewards
+            if (prevProto.BlocksPerCommitment != nextProto.BlocksPerCommitment)
+            {
+                foreach (var cycle in await Db.Cycles.Where(x => x.Index > state.Cycle).ToListAsync())
+                {
+                    cycle.NonceRevelationReward = cycle.NonceRevelationReward * nextProto.BlocksPerCommitment / prevProto.BlocksPerCommitment;
+                    cycle.VdfRevelationReward= cycle.VdfRevelationReward * nextProto.BlocksPerCommitment / prevProto.BlocksPerCommitment;
+                }
+            }
+            #endregion
         }
 
-        protected override Task RevertContext(AppState state) => Task.CompletedTask;
+        protected override Task RevertContext(AppState state) => throw new NotImplementedException();
     }
 }
